835bb5f2b7a36cf0ebff449df05762f6da4d8b38
Jevgenijs Kozlovs <jevgenijs_kozlovs@epam.com>
2022-07-04 02:50:59 +0300

    feat: move uploaded file to parsed folder

--- src/import-service/src/importFileParser.mjs
+++ src/import-service/src/importFileParser.mjs
@@ -8,65 +8,38 @@ const targetFolder = 'parsed';
 
 export const importFileParser = async (event) => {
   const objectKey = event.Records[0].s3.object.key;
+  console.log({ objectKey: objectKey })
+  try {
+    const s3Stream = await s3.getObject({
+      Bucket: BUCKET,
+      Key: objectKey,
+    }).createReadStream();
 
-  const s3Stream = await s3.getObject({
-    Bucket: BUCKET,
-    Key: objectKey,
-  }).createReadStream();
+    for await (const chunk of s3Stream.pipe(csv())) {
+      console.log(chunk);
+    }
 
+    await s3.copyObject({
+      Bucket: BUCKET,
+      CopySource: BUCKET + '/' + objectKey,
+      Key: objectKey.replace('uploaded', 'parsed'),
+    }).promise();
 
-  for await (const chunk of s3Stream.pipe(csv())) {
-    console.log(chunk)
+    await s3.deleteObject({
+      Bucket: BUCKET,
+      Key: objectKey,
+      VersionId: 'null',
+    }).promise();
+  } catch (e) {
+    console.log({ failedToParse: e })
+    return {
+      statusCode: 500,
+      body: String(e),
+    };
   }
 
   return {
     statusCode: 202,
     body: 'Parsed',
   };
-
-
-
-
-  await s3.copyObject({
-    Bucket: BUCKET,
-    CopySource: BUCKET + '/' + record.s3.object.key,
-    Key: record.s3.object.key.replace('uploaded', 'parsed'),
-  });
-
-  await s3.deleteObject({
-    Bucket: BUCKET,
-    Key: record.s3.object.key,
-  });
-  const msg = `Moved ${record.s3.object.key.split('/').pop()} from ${originFolder} to ${targetFolder}`;
-
-  console.log(msg);
-
-  return {
-    statusCode: 202,
-    body: 'Parsed',
-  };
-
-
-  // fs.createReadStream('data.csv')
-  // .pipe(csv())
-  // .on('data', (data) => results.push(data))
-  // .on('end', () => {
-  //   console.log(results);
-  //   // [
-  //   //   { NAME: 'Daffy Duck', AGE: '24' },
-  //   //   { NAME: 'Bugs Bunny', AGE: '22' }
-  //   // ]
-  // });  
 };
-
-importFileParser({
-  Records: [
-    {
-      s3: {
-        object: {
-          key: 'uploaded/flowers.csv'
-        }
-      }
-    }
-  ]
-})
